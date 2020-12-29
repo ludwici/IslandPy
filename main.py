65c12593385540336e367e1c67dbc0ee779f1ab0
Andrew <gammaclone@gmail.com>
2020-12-29 03:57:10 +0200

    test

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from IslandPy.RenderWindow import RenderWindow
+from IslandPy.Scenes.TestScene import TestScene
+
+
+class CustomScene(TestScene):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
 
 
 def main() -> None:
-    r = RenderWindow()
-    r.start()
+    s1 = TestScene("t1")
+    scenes = [s1]
+    r = RenderWindow(scenes)
+    r.start("t1")
 
 
 if __name__ == '__main__':
